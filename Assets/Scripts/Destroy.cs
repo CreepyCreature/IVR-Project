9d1c591282b060e77efecbe2abb7562c59a478aa
CreepyCreature <iverson_emin@yahoo.com>
2017-12-06 15:49:59 +0200

    Made a 1st Person Player prefab supporting 3D movement (no roll though). Testing some other stuff.

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -1,15 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Destroy : MonoBehaviour {
-    	
+public class Destroy : MonoBehaviour
+{
+    [Range(0, 1)] public float look_treshold = 0.95f;
+    [Range(0, 100)] public float distance_treshold = 5f;
+
+    private bool active_ = true;
+    private bool looked_at_ = false;
+    private bool close_enough_ = false;
+
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.GetKeyDown(KeyCode.Alpha1))
+		if (Input.GetKeyDown(KeyCode.Alpha1) && looked_at_ && close_enough_)
         {
-            Destroy(gameObject);
+            active_ = !active_;
+            GetComponent<Renderer>().enabled = active_;
         }
-	}
+
+        float dot = Vector3.Dot(
+            Vector3.Normalize(Camera.main.transform.forward),
+            Vector3.Normalize(transform.position - Camera.main.transform.position)
+            );
+        if (dot * dot > look_treshold)
+        {
+            looked_at_ = true;
+        } else { looked_at_ = false; }
+
+        if (Vector3.Magnitude(transform.position - Camera.main.transform.position) < distance_treshold)
+        {
+            close_enough_ = true;
+        } else { close_enough_ = false; }
+    }
 }
